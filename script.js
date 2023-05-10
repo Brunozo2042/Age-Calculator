5ddb24378e642b429fc31e7fe553a1e4eb555c02
Brunozo2042 <82096076+Brunozo2042@users.noreply.github.com>
2023-05-10 11:21:21 -0300

    ajuste nas consistências de datas

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ var diaA = date.getDate();
 var mesA = date.getMonth() + 1;
 var anoA = date.getFullYear();
 
-$("#inptYear").attr("max", anoA);
+var inptDay = $("#inptDay")
+var inptMonth = $("#inptMonth")
+var inptYear = $("#inptYear")
+
+inptYear.attr("max", anoA);
 
 //boolean
 var auxDay, auxMonth, auxYear;
@@ -13,25 +17,25 @@ var day, month, year;
 //respostas
 var resDay = 0, resMonth = 0, resYear = 0;
 
-function resetDay(){
-    $("#inptDay").css("border", "1px solid rgb(148, 148, 148)");
+function resetDay() {
+    inptDay.css("border", "1px solid rgb(148, 148, 148)");
     $("#lblDay").css("color", "rgb(148, 148, 148)");
     $("#errorDay").html("");
 }
 
-function resetMonth(){
-    $("#inptMonth").css("border", "1px solid rgb(148, 148, 148)");
+function resetMonth() {
+    inptMonth.css("border", "1px solid rgb(148, 148, 148)");
     $("#lblMonth").css("color", "rgb(148, 148, 148)");
     $("#errorMonth").html("");
 }
 
-function resetYear(){
+function resetYear() {
     $("#inptYear").css("border", "1px solid rgb(148, 148, 148)");
     $("#lblYear").css("color", "rgb(148, 148, 148)");
     $("#errorYear").html("");
 }
 
-function resetRes(){
+function resetRes() {
     $("#resYear").text("--")
     $("#resMonth").text("--")
     $("#resDay").text("--")
@@ -41,9 +45,9 @@ function calculate() {
 
     resetRes();
 
-    day = $("#inptDay").val();
-    month = $("#inptMonth").val();
-    year = $("#inptYear").val();
+    day = inptDay.val();
+    month = inptMonth.val();
+    year = inptYear.val();
 
     consiste();
 
@@ -63,30 +67,30 @@ function calculate() {
 
 }
 
-function calcularIdade(){
+function calcularIdade() {
 
-    while(month < mesA  - 1 || year < anoA){
+    while (month < mesA - 1 || year < anoA) {
         month++
         resMonth++
-        if(resMonth == 12){
+        if (resMonth == 12) {
             resMonth = 0
             resYear++
         }
 
-        if(month == 12){
+        if (month == 12) {
             year++
             month = 0
         }
     }
     // ajustes para o último mes
-    if(day == diaA){
+    if (day == diaA) {
         resMonth++
         resDay = 0
-    }else if(day < diaA){
+    } else if (day < diaA) {
         resMonth++
         resDay = diaA - day
     }
-    else{
+    else {
         resDay = diasNoMes(mesA - 1, anoA) + diaA - day
     }
 
@@ -96,12 +100,12 @@ function consiste() {
 
     //Consiste o dia
     if (day == "") {
-        $("#inptDay").css("border", "1px solid hsl(0, 100%, 67%)");
+        inptDay.css("border", "1px solid hsl(0, 100%, 67%)");
         $("#lblDay").css("color", "hsl(0, 100%, 67%)");
         $("#errorDay").html("This field is required!");
         auxDay = false
     } else if (day < 1 || day > 31) {
-        $("#inptDay").css("border", "1px solid hsl(0, 100%, 67%)");
+        inptDay.css("border", "1px solid hsl(0, 100%, 67%)");
         $("#lblDay").css("color", "hsl(0, 100%, 67%)");
         $("#errorDay").fadeIn().html("Must be a valid day!");
         auxDay = false
@@ -111,12 +115,12 @@ function consiste() {
 
     //Consiste o mês
     if (month == "") {
-        $("#inptMonth").css("border", "1px solid hsl(0, 100%, 67%)");
+        inptMonth.css("border", "1px solid hsl(0, 100%, 67%)");
         $("#lblMonth").css("color", "hsl(0, 100%, 67%)");
         $("#errorMonth").html("This field is required!");
         auxMonth = false
     } else if (month < 1 || month > 12) {
-        $("#inptMonth").css("border", "1px solid hsl(0, 100%, 67%)");
+        inptMonth.css("border", "1px solid hsl(0, 100%, 67%)");
         $("#lblMonth").css("color", "hsl(0, 100%, 67%)");
         $("#errorMonth").html("Must be a valid month!");
         auxMonth = false
@@ -126,12 +130,12 @@ function consiste() {
 
     //Consiste o ano
     if (year == "") {
-        $("#inptYear").css("border", "1px solid hsl(0, 100%, 67%)");
+        inptYear.css("border", "1px solid hsl(0, 100%, 67%)");
         $("#lblYear").css("color", "hsl(0, 100%, 67%)");
         $("#errorYear").html("This field is required!");
         auxYear = false
     } else if (year < 1 || year > anoA) {
-        $("#inptYear").css("border", "1px solid hsl(0, 100%, 67%)");
+        inptYear.css("border", "1px solid hsl(0, 100%, 67%)");
         $("#lblYear").css("color", "hsl(0, 100%, 67%)");
         $("#errorYear").html("Must be a valid year!");
         auxYear = false
@@ -141,63 +145,75 @@ function consiste() {
 
     if (auxDay && auxMonth && auxYear) {
 
-        //Verifica se a data existe
-        console.log(month)
-        console.log(day)
-        switch (month) {
-            //meses com até 30 dias
-            case 4: 
-            case 6: 
-            case 9: 
-            case 11:
-                if (day > 30) {
-                    $("#inptDay").css("border", "1px solid hsl(0, 100%, 67%)");
-                    $("#lblDay").css("color", "hsl(0, 100%, 67%)");
-                    $("#errorDay").html("Must be a valid date!");
-                    auxMonth = false
-                }
-                break;
-            case 2:
-                //mes com 29 dias(ano bissexto)
-                if (ehBissexto()){
-                    if (day > 29) {
-                        $("#inptDay").css("border", "1px solid hsl(0, 100%, 67%)");
-                        $("#lblDay").css("color", "hsl(0, 100%, 67%)");
-                        $("#errorDay").html("Must be a valid date!");
-                        auxMonth = false
-                    }
-                } else
-                    //mes com 28 dias(não bissexto)
-                    if (day > 28) {
-                        $("#inptDay").css("border", "1px solid hsl(0, 100%, 67%)");
-                        $("#lblDay").css("color", "hsl(0, 100%, 67%)");
-                        $("#errorDay").html("Must be a valid date!");
-                        auxMonth = false
-                    }
-                break;
+        if (dataFutura(day, month, year)) {
+            auxDay = false
+            auxMonth = false
+            auxYear = false
+            inptDay.css("border", "1px solid hsl(0, 100%, 67%)");
+            $("#lblDay").css("color", "hsl(0, 100%, 67%)");
+            inptMonth.css("border", "1px solid hsl(0, 100%, 67%)");
+            $("#lblMonth").css("color", "hsl(0, 100%, 67%)");
+            inptYear.css("border", "1px solid hsl(0, 100%, 67%)");
+            $("#lblYear").css("color", "hsl(0, 100%, 67%)");
+            $("#errorDay").html("Must be in the past!");
+        }
 
+        // console.log(day)
+        // console.log(month)
+        // console.log(year)
+
+        //Verifica se a data existe
+        if (day > diasNoMes(month, day)) {
+            inptDay.css("border", "1px solid hsl(0, 100%, 67%)");
+            $("#lblDay").css("color", "hsl(0, 100%, 67%)");
+            inptMonth.css("border", "1px solid hsl(0, 100%, 67%)");
+            $("#lblMonth").css("color", "hsl(0, 100%, 67%)");
+            inptYear.css("border", "1px solid hsl(0, 100%, 67%)");
+            $("#lblYear").css("color", "hsl(0, 100%, 67%)");
+            $("#errorDay").html("Must be a valid date!");
+            auxMonth = false
         }
     }
 }
 
-function ehBissexto(year){
-    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
+function ehBissexto(year) {
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
         return true
-    }else{
+    } else {
         return false
     }
 }
 
-function diasNoMes(mes,ano){
-    if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12){
+function diasNoMes(mes, ano) {
+    if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
         return 31
-    }else{
-        if(mes == 4 || mes == 6 || mes == 9 || mes == 11){
+    } else {
+        if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
             return 30
-        }else if(ehBissexto(ano)){
+        } else if (ehBissexto(ano)) {
             return 29
-        }else{
+        } else {
             return 28
         }
-    } 
+    }
+}
+
+function dataFutura(dia, mes, ano) {
+    if (ano > anoA) {
+        return true
+    } else if (ano === anoA) {
+        if (mes > mesA) {
+            return true
+        } else if (mes === mesA) {
+            if (dia >= diaA) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
 }
