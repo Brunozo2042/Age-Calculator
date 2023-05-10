bb9ee31a82c96c53850c871c061d464a4262ed7e
Brunozo2042 <82096076+Brunozo2042@users.noreply.github.com>
2023-05-09 21:10:21 -0300

    calcula idade

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 var date = new Date();
+//data atual
 var diaA = date.getDate();
 var mesA = date.getMonth() + 1;
 var anoA = date.getFullYear();
 
 $("#inptYear").attr("max", anoA);
 
+//boolean
 var auxDay, auxMonth, auxYear;
+//datas digitadas
 var day, month, year;
+//respostas
+var resDay = 0, resMonth = 0, resYear = 0;
+
+function resetDay(){
+    $("#inptDay").css("border", "1px solid rgb(148, 148, 148)");
+    $("#lblDay").css("color", "rgb(148, 148, 148)");
+    $("#errorDay").html("");
+}
+
+function resetMonth(){
+    $("#inptMonth").css("border", "1px solid rgb(148, 148, 148)");
+    $("#lblMonth").css("color", "rgb(148, 148, 148)");
+    $("#errorMonth").html("");
+}
+
+function resetYear(){
+    $("#inptYear").css("border", "1px solid rgb(148, 148, 148)");
+    $("#lblYear").css("color", "rgb(148, 148, 148)");
+    $("#errorYear").html("");
+}
+
+function resetRes(){
+    $("#resYear").text("--")
+    $("#resMonth").text("--")
+    $("#resDay").text("--")
+}
 
 function calculate() {
+
+    resetRes();
 
     day = $("#inptDay").val();
     month = $("#inptMonth").val();
@@ -18,30 +49,45 @@ function calculate() {
 
     if (auxDay && auxMonth && auxYear) {
 
+        //Variáveis resposta
+        resDay = 0
+        resMonth = 0
+        resYear = 0
 
-
-        var resDay, resMonth, resYear;
-
-        resYear = anoA - year;
-
-        if (mesA < month) {
-            resYear--;
-            resMonth = 12 - (month - mesA)
-        } else {
-            resMonth = mesA - month
-        }
-
-        if (diaA < day) {
-            resMonth--;
-            resDay = 30 - (day - diaA)
-        } else {
-            resDay = diaA - day
-        }
-
+        calcularIdade();
 
         $("#resYear").text(resYear)
         $("#resMonth").text(resMonth)
         $("#resDay").text(resDay)
+    }
+
+}
+
+function calcularIdade(){
+
+    while(month < mesA  - 1 || year < anoA){
+        month++
+        resMonth++
+        if(resMonth == 12){
+            resMonth = 0
+            resYear++
+        }
+
+        if(month == 12){
+            year++
+            month = 0
+        }
+    }
+    // ajustes para o último mes
+    if(day == diaA){
+        resMonth++
+        resDay = 0
+    }else if(day < diaA){
+        resMonth++
+        resDay = diaA - day
+    }
+    else{
+        resDay = diasNoMes(mesA - 1, anoA) + diaA - day
     }
 
 }
@@ -57,7 +103,7 @@ function consiste() {
     } else if (day < 1 || day > 31) {
         $("#inptDay").css("border", "1px solid hsl(0, 100%, 67%)");
         $("#lblDay").css("color", "hsl(0, 100%, 67%)");
-        $("#errorDay").html("Must be a valid day!");
+        $("#errorDay").fadeIn().html("Must be a valid day!");
         auxDay = false
     } else {
         auxDay = true
@@ -94,10 +140,16 @@ function consiste() {
     }
 
     if (auxDay && auxMonth && auxYear) {
+
         //Verifica se a data existe
+        console.log(month)
+        console.log(day)
         switch (month) {
             //meses com até 30 dias
-            case 4, 6, 9, 11:
+            case 4: 
+            case 6: 
+            case 9: 
+            case 11:
                 if (day > 30) {
                     $("#inptDay").css("border", "1px solid hsl(0, 100%, 67%)");
                     $("#lblDay").css("color", "hsl(0, 100%, 67%)");
@@ -107,7 +159,7 @@ function consiste() {
                 break;
             case 2:
                 //mes com 29 dias(ano bissexto)
-                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                if (ehBissexto()){
                     if (day > 29) {
                         $("#inptDay").css("border", "1px solid hsl(0, 100%, 67%)");
                         $("#lblDay").css("color", "hsl(0, 100%, 67%)");
@@ -126,4 +178,26 @@ function consiste() {
 
         }
     }
+}
+
+function ehBissexto(year){
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
+        return true
+    }else{
+        return false
+    }
+}
+
+function diasNoMes(mes,ano){
+    if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12){
+        return 31
+    }else{
+        if(mes == 4 || mes == 6 || mes == 9 || mes == 11){
+            return 30
+        }else if(ehBissexto(ano)){
+            return 29
+        }else{
+            return 28
+        }
+    } 
 }
